f248ffb50c218086710ac676ceb2f184a6d58e77
Paulo Augusto Silva <p13lgst@gmail.com>
2022-03-27 07:30:22 -0300

    Add favicons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,13 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Anton&family=Open+Sans:wght@700;800&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#050024" />
+        <meta name="msapplication-TileColor" content="#050024" />
+        <meta name="theme-color" content="#050024" />
       </Head>
       <body>
         <Main />
